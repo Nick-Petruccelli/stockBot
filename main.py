608e54cd7b2409d006d03fb5f7a17c5edb857303
NickPetruccelli <NickPetro18@gmail.com>
2024-11-20 16:52:56 -0500

    tiingo request

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import requests
 
 key = ""
 with open("tiingoKey.txt", "r") as data:
-    key = data.readline()
-print(key)
+    key = data.read().strip()
+
+headers = {
+            'Content-Type': 'application/json'
+}
+requestResponse = requests.get(f"https://api.tiingo.com/tiingo/crypto/prices?tickers=btcusd&startDate=2019-01-02&resampleFreq=5min&token={key}", headers=headers)
+print(requestResponse.json())
